b7bd3f77cc5ee89c687cd8d808664ec777f268d8
hammefatal <hamsunhong@gmail.com>
2024-12-06 13:38:03 +0900

    Add files via upload
    init push

---
+++ src/main/java/hammefatal/toyproj/hexagonalArchitecture/HexagonalArchitectureApplication.java
@@ -0,0 +1,13 @@
+package hammefatal.toyproj.hexagonalArchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalArchitectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hammefatal/toyproj/hexagonalArchitecture/HexagonalArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package hammefatal.toyproj.hexagonalArchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
